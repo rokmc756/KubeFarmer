c13ae50f2e971eaf611b7a05c72c119e26a61519
Jack Moon <rokmc756@gmail.com>
2024-06-04 23:21:09 +0900

    Updated for Ubuntu Linux

--- shutdown-vms.sh
+++ shutdown-vms.sh
@@ -1,2 +1,5 @@
+
 ssh root@192.168.0.90 "shutdown -h now"
-for i in `seq 1 5`; do ssh root@192.168.17$i "shutdown -h now" ;done
+
+for i in `seq 1 5`; do ssh root@192.168.6$i "shutdown -h now" ;done
+
