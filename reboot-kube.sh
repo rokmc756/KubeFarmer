b4fc05479a1ef958589018640daca44a57907c67
Jack Moon <rokmc756@gmail.com>
2025-02-03 19:58:03 +0900

    Mdofied Reinitialize K8S

--- reboot-kube.sh
+++ reboot-kube.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+root_pass="changeme"
+
 for i in $(seq 71 77)
 do
-    ssh root@192.168.1.$i reboot
+
+    sshpass -p "$root_pass" ssh -o StrictHostKeyChecking=no root@192.168.1.$i reboot
+
 done
 
